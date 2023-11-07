d024bb27a45629bb61e57670db023e74e99d2828
GabrielFEICC <148162892+GabrielFEICC@users.noreply.github.com>
2023-11-07 22:46:58 +0000

    comeco

---
+++ biblioteca.h
@@ -0,0 +1,30 @@
+
+
+#ifndef PROJETO_PROJ_H
+#define PROJETO_PROJ_H
+
+
+//para criar um nome para cada funcao que usaremos na proj.c
+typedef struct {
+    int prioridade;
+    char categoria[100];
+    char descricao[300];
+} Tarefa;
+
+typedef struct {
+    Tarefa tarefas[100];
+    int qtd;
+} ListaDeTarefas;
+
+//funcoes de cadastrar, deletar e listar tarefas
+int cadastrarTarefa(ListaDeTarefas *lt);
+int deletarTarefas(ListaDeTarefas *lt);
+int listarTarefas(ListaDeTarefas lt);
+
+//funcao para mostrar o menu para o usuario
+void printMenu();
+//para salvar as tarefas no arquivo
+int salvarTarefas(ListaDeTarefas *lt, char *arquivo);
+//para carregar as tarefas no arquivos
+int carregarTarefas(ListaDeTarefas *lt, char *arquivo);
+#endif //PROJETO_PROJ_H
